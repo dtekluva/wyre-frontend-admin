799b6f0c126918c9e023c5907e908811283b8459
wazkayd <wazkayd.sbs@gmail.com>
2023-06-25 22:05:29 +0100

    reminder mail done

--- src/pages/authPages/FillDieselEntry.js
+++ src/pages/authPages/FillDieselEntry.js
@@ -11,6 +11,8 @@ import { APIServiceNoAuth } from '../../config/api/apiConfig';
 import EnvData from '../../config/EnvData';
 import FailedEmail from './FailedEmail';
 import SuccessEmail from './SuccessEmail';
+import FilledEmail from './FilledEmail';
+import NoUrlEmail from './NoUrlEmail';
 
 
 function FillDieselEntry(props) {
@@ -19,41 +21,80 @@ function FillDieselEntry(props) {
   const [formData, setFormData] = useState();
   const [openSucsModal, setOpenSucsModal] = useState(false);
   const [openFailModal, setOpenFailModal] = useState(false);
+  const [openNoUrlModal, setOpenNoUrlModal] = useState(false);
+  const [openCompletedEntryModal, setOpenCompletedEntryModal] = useState(false);
   const [responseMsg, setResponseMsg] = useState();
+  const [checkResponseMsg, setCheckResponseMsg] = useState();
+  const [checkMailStatus, setCheckMailStatus] = useState();
   const [loading, setIsLoading] = useState(false);
-  const [branchData, setBranchData] = useState(false);
+  const [branchIdInfo, setBranchIdInfo] = useState(false);
+  const [handleEj, setHandleEj] = useState();
+  const [handleEf, setHandleEf] = useState();
 
   useEffect(() => {
     const branch = searchParams.get('ef');
     const entryData = searchParams.get('ej');
 
+    if (!branch || !entryData) {
+     return setOpenNoUrlModal(true)
+    }
+    
+    setHandleEf(branch)
+    setHandleEj(entryData)
+
+    if (checkMailStatus === false) {
+      return setOpenCompletedEntryModal(true)
+    }
     // Creating the buffer object with utf8 encoding
     let branchString = Buffer.from(branch, "base64").toString();
     const dieselEntryString = Buffer.from(entryData, "base64").toString();
+    let branchIdInfo
 
     if (branchString) {
       const ab = branchString.replace(/'/g, '"');
-      const branchData = JSON.parse(ab);
-
-      setBranchData(branchData);
+      branchIdInfo = JSON.parse(ab);
+      setBranchIdInfo(branchIdInfo);
     }
 
     if (dieselEntryString) {
       const yy = dieselEntryString.replace(/'/g, '"');
-      const data = JSON.parse(yy)
-    
+      const data = JSON.parse(yy)          
  
-      // format the data to be in the required format
-      const myData = Object.keys(data).map((key) => {
-        // const keyData
-        return {
-          'date': key, diesel_consumed: data[key],
-          day: moment([key], 'YYYY-MM-DD').format('dddd')
-        }
-      }).filter((newData) => !newData.diesel_consumed)
-      setFormData(myData);
+      const branchDateData = Object.keys(data).map((key) => {
+        return key 
+      })
+      onCheckingReport(branchIdInfo, branchDateData)     
     }
   }, [])
+
+  const onCheckingReport = async (branchIdInfo, bodyData) => {
+    const params = {
+      ej : bodyData
+    }
+        try {
+          const testChecking = await APIServiceNoAuth
+           .post(`${EnvData.REACT_APP_API_BASE_URL}/api/v1/reminder_mail_check/${branchIdInfo.id}/`, params);
+           const dieselEntryString = Buffer.from(testChecking.data.branch_info, "base64").toString();
+           if (dieselEntryString) {
+            const yy = dieselEntryString.replace(/'/g, '"');
+            const data = JSON.parse(yy)            
+            // format the data to be in the required format
+            const myData = Object.keys(data).map((key) => {
+              return {
+                'date': key, diesel_consumed: data[key],
+                day: moment([key], 'YYYY-MM-DD').format('dddd')
+              }
+            }).filter((newData) => !newData.diesel_consumed)
+            setFormData(myData);
+            
+            setCheckMailStatus(testChecking.data.status)
+            setCheckResponseMsg(testChecking.data.message)
+          }
+        } catch (error) {
+          setResponseMsg(error.response.data.error)
+        }
+    
+  }
 
 
   const onFormSubmit = async (values) => {
@@ -63,7 +104,7 @@ function FillDieselEntry(props) {
     try {
       setIsLoading(true)
       const submitForm = await APIServiceNoAuth
-        .post(`${EnvData.REACT_APP_API_BASE_URL}/api/v1/post_weekly_diesel_usage/${branchData.id}/`, values);
+        .post(`${EnvData.REACT_APP_API_BASE_URL}/api/v1/post_weekly_diesel_usage/${branchIdInfo.id}/`, values);
       setResponseMsg(submitForm.data.message)
       setIsLoading(false)
       
@@ -73,7 +114,6 @@ function FillDieselEntry(props) {
       setIsLoading(false)
       handleFailModal()
     }
-
   }
 
 
@@ -119,107 +159,122 @@ function FillDieselEntry(props) {
 
   return (
     <>
-      <div className="diesel_container_1">
-        <div className="diesel_container_2">
-          <div
-            style={{
-              height: "100%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            className="cost-tracker-forms-content-wrapper"
+      {!handleEf || !handleEj ? (
+        <Modal
+          open={openNoUrlModal}
+          onOk={() => setOpenNoUrlModal(false)}
+          onCancel={() => setOpenNoUrlModal(false)}
+          width={400}
+          footer={null}
+        >
+          <NoUrlEmail setModal={setOpenNoUrlModal} />
+        </Modal>
+      ) : checkMailStatus === false ? (
+        <div>
+          <Modal
+            open={openCompletedEntryModal}
+            onOk={() => setOpenCompletedEntryModal(false)}
+            onCancel={() => setOpenCompletedEntryModal(false)}
+            width={400}
+            footer={null}
           >
-            <Spin
-              spinning={
-                loading
-              }
+            <FilledEmail
+              setModal={setOpenCompletedEntryModal}
+              checkResponseMsg={checkResponseMsg}
+            />
+          </Modal>
+        </div>
+      ) : (
+        <div className="diesel_container_1">
+          <div className="diesel_container_2">
+            <div
+              style={{
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              className="cost-tracker-forms-content-wrapper"
             >
-              <h1 className="center-main-heading">Diesel Register</h1>
-              <h2
-                className=""
-                style={{ display: "flex", justifyContent: "center" }}
-              >
-                {branchData.name}
-              </h2>
-              <p style={{ display: "flex", justifyContent: "center" }}>
-                Please fill in the required data below
-              </p>
-              {
-                <section className="cost-tracker-form-section">
-                  <Form
-                    form={form}
-                    name="basic"
-                    labelCol={{ span: 8 }}
-                    wrapperCol={{ span: 16 }}
-                    autoComplete="off"
-                    className="cost-tracker-form"
-                    onFinish={onFormSubmit}
-                  >
-                    <Table
-                      className="table-striped-rows row-head"
-                      columns={columns}
-                      dataSource={formData}
-                      // loading={loading}
-                      rowKey={(record) => record.id}
-                      pagination={false}
-                      footer={() => ``}
-                    />
-                    <div
-                      className=""
-                      style={{ display: "flex", justifyContent: "center" }}
+              <Spin spinning={loading}>
+                <h1 className="center-main-heading">Diesel Register</h1>
+                <h2
+                  className=""
+                  style={{ display: "flex", justifyContent: "center" }}
+                >
+                  {branchIdInfo.name}
+                </h2>
+                <p style={{ display: "flex", justifyContent: "center" }}>
+                  Please fill in the required data below
+                </p>
+                {
+                  <section className="cost-tracker-form-section">
+                    <Form
+                      form={form}
+                      name="basic"
+                      labelCol={{ span: 8 }}
+                      wrapperCol={{ span: 16 }}
+                      autoComplete="off"
+                      className="cost-tracker-form"
+                      onFinish={onFormSubmit}
                     >
-                      <button
-                        className="generic-submit-button-other cost-tracker-form-submit-button color"
-                        style={{ color: "FFCC4D" }}
+                      <Table
+                        className="table-striped-rows row-head"
+                        columns={columns}
+                        dataSource={formData}
+                        // loading={loading}
+                        rowKey={(record) => record.id}
+                        pagination={false}
+                        footer={() => ``}
+                      />
+                      <div
+                        className=""
+                        style={{ display: "flex", justifyContent: "center" }}
                       >
-                        Submit
-                      </button>
-                    </div>
-                  </Form>
-                </section>
-              }
-            </Spin>
-            {/* Success Modal */}
-            <Modal
-              open={openSucsModal}
-              onOk={() => setOpenSucsModal(false)}
-              onCancel={() => setOpenSucsModal(false)}
-              width={400}
-              footer={null}
-            >
-              <SuccessEmail
-                setModal={setOpenSucsModal}
-                responseMsg={responseMsg}
-              />
-            </Modal>
+                        <button
+                          className="generic-submit-button-other cost-tracker-form-submit-button color"
+                          style={{ color: "FFCC4D" }}
+                        >
+                          Submit
+                        </button>
+                      </div>
+                    </Form>
+                  </section>
+                }
+              </Spin>
+              {/* Success Modal */}
+              <Modal
+                open={openSucsModal}
+                onOk={() => setOpenSucsModal(false)}
+                onCancel={() => setOpenSucsModal(false)}
+                width={400}
+                footer={null}
+              >
+                <SuccessEmail
+                  setModal={setOpenSucsModal}
+                  responseMsg={responseMsg}
+                />
+              </Modal>
 
-            {/* Failed Modal */}
-            <Modal
-              open={openFailModal}
-              onOk={() => setOpenFailModal(false)}
-              onCancel={() => setOpenFailModal(false)}
-              width={400}
-              footer={null}
-            >
-              <FailedEmail
-                setModal={setOpenFailModal}
-                responseMsg={responseMsg}
-              />
-            </Modal>
+              {/* Failed Modal */}
+              <Modal
+                open={openFailModal}
+                onOk={() => setOpenFailModal(false)}
+                onCancel={() => setOpenFailModal(false)}
+                width={400}
+                footer={null}
+              >
+                <FailedEmail
+                  setModal={setOpenFailModal}
+                  responseMsg={responseMsg}
+                />
+              </Modal>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
 
-
 export default FillDieselEntry
-
-
-
-
-
-
-// end of script
