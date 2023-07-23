d1f727f5e47960940ef1ba3ef8b2a86d692c8869
idriskelani <idmega2000@gmail.com>
2023-07-24 00:53:17 +0100

    fix condition to show form select

--- ab.jsx
+++ ab.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-restricted-globals */
 import React, { useEffect, useState } from 'react';
+// import { Spin, Form, notification, Select, DatePicker } from 'antd';
 import { CaretDownFilled } from '@ant-design/icons';
 import { Buffer } from 'buffer';
 
@@ -12,39 +13,45 @@ import { APIServiceNoAuth } from '../../config/api/apiConfig';
 import EnvData from '../../config/EnvData';
 import FailedEmail from './FailedEmail';
 import SuccessEmail from './SuccessEmail';
+import FilledEmail from './FilledEmail';
 import NoUrlEmail from './NoUrlEmail';
 
 import jwtDecode from 'jwt-decode';
 
+const { RangePicker } = DatePicker
 
 function UtilityPage(props) {
   const [selectionForm] = Form.useForm();
   const [utilityType, setUtilityType] = useState(null);
   const [form] = Form.useForm();
   const [searchParams] = useSearchParams();
+  const [formData, setFormData] = useState();
   const [openSucsModal, setOpenSucsModal] = useState(false);
   const [openFailModal, setOpenFailModal] = useState(false);
   const [openNoUrlModal, setOpenNoUrlModal] = useState(false);
   const [responseMsg, setResponseMsg] = useState();
   const [loading, setIsLoading] = useState(false);
   const [branchIdInfo, setBranchIdInfo] = useState(false);
-  const [handledi, setHandleDi] = useState(null);
-  const [handledj, setHandleDj] = useState(null);
-  const [handleExpiration, setHandleExpiration] = useState(null);
+  const [handledi, setHandleDi] = useState();
+  const [handledj, setHandleDj] = useState();
+  const [handleExpiration, setHandleExpiration] = useState();
 
   useEffect(() => {
     const branch = searchParams.get('dj');
     const formEntry = searchParams.get('di');
-    if (!branch || !formEntry) {
-      return setOpenNoUrlModal(true)
-    }
-    setHandleDj(branch)
-    setHandleDi(formEntry)
     const fetchExpiration = jwtDecode(formEntry);
     const currentTime = (Date.now() - 30000) / 1000
 
-    const handleExp = fetchExpiration?.exp < currentTime
+    const handleExp = fetchExpiration?.exp > currentTime
+    console.log("checking for exp", handleExp);
     setHandleExpiration(handleExp)
+
+    if (!branch || !formEntry) {
+      return setOpenNoUrlModal(true)
+    }
+
+    setHandleDj(branch)
+    setHandleDi(formEntry)
 
     let branchString = Buffer.from(branch, "base64").toString();
     let branchIdInfo
@@ -130,6 +137,7 @@ function UtilityPage(props) {
     setUtilityType(selected);
   };
 
+  const { Option } = Select;
 
   const utilities = [
     {
@@ -164,7 +172,7 @@ function UtilityPage(props) {
   return (
     <>
       <>
-        {(!handledj || !handledi || handleExpiration) && (
+        {(!handledj || !handledi) && (
           <Modal
             open={openNoUrlModal}
             onOk={() => setOpenNoUrlModal(false)}
@@ -174,11 +182,12 @@ function UtilityPage(props) {
           >
             <NoUrlEmail setModal={setOpenNoUrlModal} />
           </Modal>
-        )}
+        )
+        }
       </>
       {
         utilityType === 'prepaid' ? (
-          <>
+          <> (
             <div className="diesel_container_1">
               <div className="diesel_container_2">
                 <div
@@ -200,7 +209,7 @@ function UtilityPage(props) {
                     >
                       {branchIdInfo.name}
                     </h2>
-                    <p style={{ fontFamily: "montssera", display: "flex", justifyContent: "center" }}>
+                    <p style={{ display: "flex", justifyContent: "center" }}>
                       Please Input Your Pre Paid Utility Bills
                     </p>
                     <section className="cost-tracker-form-section">
@@ -304,10 +313,12 @@ function UtilityPage(props) {
                 </div>
               </div>
             </div>
+            )
           </>
         ) :
           utilityType === 'postPaid' ? (
             <>
+              (
               <div className="diesel_container_1">
                 <div className="diesel_container_2">
                   <div
@@ -329,7 +340,7 @@ function UtilityPage(props) {
                       >
                         {branchIdInfo.name}
                       </h2>
-                      <p style={{ fontFamily: "montssera", display: "flex", justifyContent: "center" }}>
+                      <p style={{ display: "flex", justifyContent: "center" }}>
                         Please Input Your Monthly Post Paid Utility Bills
                       </p>
                       <section className="cost-tracker-form-section">
@@ -366,7 +377,7 @@ function UtilityPage(props) {
                               { required: true, message: "Please enter the date period" },
                             ]}
                           >
-                            <DatePicker picker='month'
+                            <RangePicker
                               className="signup-login-contact-input outlined-input"
                               size="large"
                             />
@@ -433,9 +444,9 @@ function UtilityPage(props) {
                   </div>
                 </div>
               </div>
+              )
             </>
           ) : (
-
             <div
               style={{
                 height: "500px",
@@ -445,44 +456,41 @@ function UtilityPage(props) {
               }}
               className="cost-tracker-forms-content-wrapper"
             >
-              {(handledj && handledi && !handleExpiration) &&
-                (
-                  <div>
-                    <h1 className="center-main-heading">Please Select A Utility Type</h1>
-                    <section className="cost-tracker-form-section">
-                      <Form
-                        form={selectionForm}
-                        name="basic"
-                        labelCol={{ span: 8 }}
-                        wrapperCol={{ span: 16 }}
-                        className="cost-tracker-form"
-                        onFinish={onSelectSubmit}
-                      >
-                        <div className="add-cclient-form-inputs-wrapper">
-                          <div className="add-client-input-container-half">
-                            <Form.Item
-                              name="utility"
-                              label="Utility"
-                              labelCol={{ span: 24 }}
-                              validateTrigger={["onChange", "onBlur"]}
-                              rules={[
-                                { required: true, message: "Please select a utility type" },
-                              ]}
-                            >
-                              {/* <Select size='large' className=' outlined-input_second' /> */}
-                              {utilitySelector}
-                            </Form.Item>
-                          </div>
-                        </div>
-                        <div className="add_user_form_btn_align">
-                          <button className="generic-submit-button cost-tracker-form-submit-button">
-                            Click here
-                          </button>
-                        </div>
-                      </Form>
-                    </section>
-                  </div>
-                )}
+              <div>
+                <h1 className="center-main-heading">Please Select A Utility Type</h1>
+                <section className="cost-tracker-form-section">
+                  <Form
+                    form={selectionForm}
+                    name="basic"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    className="cost-tracker-form"
+                    onFinish={onSelectSubmit}
+                  >
+                    <div className="add-cclient-form-inputs-wrapper">
+                      <div className="add-client-input-container-half">
+                        <Form.Item
+                          name="utility"
+                          label="Utility"
+                          labelCol={{ span: 24 }}
+                          validateTrigger={["onChange", "onBlur"]}
+                          rules={[
+                            { required: true, message: "Please select a utility type" },
+                          ]}
+                        >
+                          {/* <Select size='large' className=' outlined-input_second' /> */}
+                          {utilitySelector}
+                        </Form.Item>
+                      </div>
+                    </div>
+                    <div className="add_user_form_btn_align">
+                      <button className="generic-submit-button cost-tracker-form-submit-button">
+                        Click here
+                      </button>
+                    </div>
+                  </Form>
+                </section>
+              </div>
             </div>
           )
       }
